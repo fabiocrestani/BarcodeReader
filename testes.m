f8a12d05e0987fd429d74e8adf00c4cf0b80c660
Fabio Crestani <crestani.fabio@gmail.com>
2017-06-07 18:52:14 -0300

    Delete testes.m

--- testes.m
+++
@@ -1,7 +0,0 @@
-loaded = load('miniOCR/oito.mat');
-oito = loaded.firstDigitExtracted;
-figure;
-imshow(oito);
-
-% TODO gerar códigos de barras para todos os números de 0 a 9 para poder
-% determinar o miniOCR do primeiro dígito
